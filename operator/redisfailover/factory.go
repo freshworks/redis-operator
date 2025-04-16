220c39e8f591cb9b456e958433bb1d4819549e13
emsaravanan <emsaravana@gmail.com>
2025-04-16 16:16:21 +0530

    syntax fix

--- operator/redisfailover/factory.go
+++ operator/redisfailover/factory.go
@@ -43,12 +43,12 @@ func New(cfg Config, k8sService k8s.Services, k8sClient kubernetes.Interface, lo
 	kooperLogger := kooperlogger{Logger: logger.WithField("operator", "redisfailover")}
         
         lockCfg := &leaderelection.LockConfig{
-		LeaseDuration: 60 * time.Second,
-		RenewDeadline: 55 * time.Second,
-		RetryPeriod:   2 * time.Second,
+	    LeaseDuration: 60 * time.Second,
+	    RenewDeadline: 55 * time.Second,
+	    RetryPeriod:   2 * time.Second,
 	}
 	// Leader election service.
-	leSVC, err := leaderelection.New(lockKey, lockNamespace, k8sClient, kooperLogger)
+	leSVC, err := leaderelection.New(lockKey, lockNamespace, lockCfg, k8sClient, kooperLogger)
 	if err != nil {
 		return nil, err
 	}
